aa1b0cb27c5887ddc8b3260ea1f5e5ea71c3954e
BaldyAsh <antongrigorjev2010@gmail.com>
2019-03-14 18:31:42 +0300

    init

---
+++ ios/PowersOfTau/PowersOfTau/PowersOfTau-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PowersOfTau-Bridging-Header.h
+//  PowersOfTau
+//
+//  Created by Anton on 14/03/2019.
+//  Copyright © 2019 TheMatter. All rights reserved.
+//
+
+#ifndef PowersOfTau_Bridging_Header_h
+#define PowersOfTau_Bridging_Header_h
+
+
+#endif /* PowersOfTau_Bridging_Header_h */
---
+++ ios/PowersOfTau/PowersOfTau/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PowersOfTau
+//
+//  Created by Anton on 14/03/2019.
+//  Copyright © 2019 TheMatter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
